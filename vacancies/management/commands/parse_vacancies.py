91ddc0c33117d81406494cc1e6c10722f994e3f2
Eterexx <144895838+malakall@users.noreply.github.com>
2024-07-06 08:18:02 +0300

    first commit

--- vacancies/management/commands/parse_vacancies.py
+++ vacancies/management/commands/parse_vacancies.py
@@ -2,6 +2,13 @@ import requests
 from django.core.management.base import BaseCommand
 from vacancies.models import Vacancy
 
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from django.core.management import call_command
+
+
+
 class Command(BaseCommand):
     help = 'Parse vacancies from HH.ru and save to database'
 
@@ -54,3 +61,35 @@ class Command(BaseCommand):
             )
 
         self.stdout.write(self.style.SUCCESS(f'Data collected and filtered. Total vacancies: {len(vacancies_data)}'))
+
+
+
+class VacancyListView(View):
+    def get(self, request):
+        name_filter = request.GET.get('name')
+        salary_from_filter = request.GET.get('salary_from')
+        salary_to_filter = request.GET.get('salary_to')
+        address_filter = request.GET.get('address')
+
+        vacancies = Vacancy.objects.all()
+
+        if name_filter:
+            vacancies = vacancies.filter(name__icontains=name_filter)
+        if salary_from_filter:
+            vacancies = vacancies.filter(salary_from__gte=salary_from_filter)
+        if salary_to_filter:
+            vacancies = vacancies.filter(salary_from__lte=salary_to_filter)
+        if address_filter:
+            vacancies = vacancies.filter(address__icontains=address_filter)
+
+        return render(request, 'vacancies/vacancy_list.html', {'vacancies': vacancies})
+
+    def post(self, request):
+        pages = request.POST.get('pages', 1)
+        call_command('parse_vacancies', pages=pages)
+        return JsonResponse({'status': 'Database updated'})
+
+class VacancyCountView(View):
+    def get(self, request):
+        count = Vacancy.objects.count()
+        return JsonResponse({'count': count})
